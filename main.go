a8dbb194cb6970641067bd95dbc94c60cbe27665
s7electric <samiacobazzi@gmail.com>
2026-06-14 20:27:57 -0400

    completed rev.1 parse()

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/user"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -26,6 +28,49 @@ func main() {
 
 		var input string
 		fmt.Scanln(&input)
-		fmt.Println(input)
+
 	}
+}
+
+func eval(input string) (string, error) {
+
+}
+
+func parse(input string) ([]string, error) {
+	var output []string
+	for i := 0; i < len(input); {
+		// 1. consume all spaces
+		for input[i] == ' ' {
+			i++
+		}
+		// 2. handle next token
+		if input[i] == '$' {
+			if c_bracket := find_matching_c_bracket(input[i+2:]); c_bracket == -1 || input[i+1] != '(' {
+				return nil, errors.New("syntax error - incorrect usage of '$()'")
+			} else {
+				output = append(output, input[i:c_bracket+1])
+				i = c_bracket + 1
+			}
+		} else {
+			next := strings.IndexByte(input, ' ')
+			output = append(output, input[i:next])
+			i = next
+		}
+	}
+	return output, nil
+}
+
+func find_matching_c_bracket(str string) int {
+	var balance int = 1
+	for i := 0; i < len(str); i++ {
+		if str[i] == '(' {
+			balance++
+		} else if str[i] == ')' {
+			balance--
+		}
+		if balance == 0 {
+			return i
+		}
+	}
+	return -1
 }
